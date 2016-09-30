f9f1d24345b91e0a0185f860a2e3022034ec323a
Anthony Roussel <anthony.roussel@insa-lyon.fr>
2016-09-30 16:34:25 +0200

    convolutional interleaving story ( card#35 )

---
+++ interleaving_story.m
@@ -0,0 +1,27 @@
+%% configuration
+nrows = 3; % use 3 delay lines
+slope = 2; % delay are 0, 4, 8, 12
+delay = nrows * ( nrows - 1 ) * slope;
+
+%% interleaving
+% msg = randi( [0 1], 1, 6 ); % original data
+msg = [ 0 1 0 1 1 0 1 1 1 1 1 1 0 1 0 1 0 1  ];
+
+disp( 'msg=' );
+disp( msg );
+
+msg = [ msg zeros( 1, delay ) ];
+intrvled = convintrlv( msg, nrows, slope );
+
+disp( 'intrvled=' );
+disp( intrvled );
+
+%% deinterleaving
+% convolutional deinterleaver
+deintrlved = convdeintrlv( intrvled, nrows, slope );
+
+% omit the first d symbols of recovered data
+deintrlved = deintrlved( (delay+1):end );
+
+disp( 'deintrlved=' );
+disp( deintrlved );

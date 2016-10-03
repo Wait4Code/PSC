b6f4a01a1abfc53008427efa2e5a9e7e38a90e13
Anthony Roussel <anthony.roussel@insa-lyon.fr>
2016-10-03 17:27:40 +0200

    fix bad encoding for gene bits matlab file

--- gene_bits.m
+++ gene_bits.m
@@ -1,9 +1,9 @@
-function suite_bits=gene_bits(N,p0)
+function suite_bits = gene_bits( N, p0 )
 
-% La fonction gene_bits g�n�re une suite binaire al�atoire de longueur N.
+% La fonction gene_bits génère une suite binaire aléatoire de longueur N.
 
 % N est la longueur de la suite.
-% p0 est la probabilit� d'avoir un 0 (p0 = 1/2 <=> cas �quiprobable).
+% p0 est la probabilité d'avoir un 0 (p0 = 1/2 <=> cas équiprobable).
 
 
 suite_ini=sign(rand(1,N)-p0*ones(1,N)); % On obtient une suite de -1,0,1
@@ -15,4 +15,4 @@ for i=1:N
 end
 
 % Et on transpose finalement la suite de -1,1 en une suite de 0,1
-suite_bits=0.5*(suite_ini+1); 
+suite_bits=0.5*(suite_ini+1);

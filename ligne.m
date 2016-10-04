4522a1165338c04caa2bd4000e3acf1aa49b671d
unknown <Perso@PC-LTERMAT.fingerprint.lan>
2016-10-04 17:03:11 +0200

    Bruitage canal (blanc et ponctuel)

--- ligne.m
+++ ligne.m
@@ -10,40 +10,12 @@ snr         [entier]    valeur du SNR souhaité par canal en dB
 
 ---RETURN---
 sig_bruite  [vecteur] signal en sortie de ligne
+
 %}
 
 
 
 
 sig_att = conv2(sig_mod,h);
-sig_bruite = awgn(sig_att,snr); 
-sig_bruite=sig_att;
-%{
-t=0:0.1:6*pi;
-sig=sin(t);
-sig2=sin(2*t);
-
-plot(t,sig);
-sig_f=abs(fft(sig));
-sig_f2=abs(fft(sig2));
-
-sig_f=sig_f+sig_f2;
-l=length(t);
-f=100*(0:l/2-1)/l;
-sig_f=sig_f(1:l/2);
-figure(2);
-plot(f,sig_f);
-
-sig_i=abs(ifft(sig_f));
-figure(3);
-plot(f,sig_i);
-
-snr=1;
-for i = 1:length(sig_i) 
-    sig_i(i)=awgn(sig_i(i),snr);
-    snr=snr+i*2*snr*10;
-end
-
-figure(5);
-plot(f,abs(ifft(sig_f)));
-%}
+t=0:1:607;
+sig_bruite = awgn(sig_att,snr)+16*sin(1e5*t);
--- testeur_ligne.m
+++ testeur_ligne.m
@@ -1,16 +1,16 @@
-f_transfert
-
-%Au démodulatateur, on a Se (Signal d'entrée)
-
+%fonction de transfert du canal
+h=f_transfert(3000,0.0005);
 
 t=0:0.1:6*pi;
-sig1=sin(1e9*t);
+sig1=sin(1e10*t);
 sig2=sin(t);
+sig3=sin(1e5*t);
 
-sig=sig1+sig2;
+sig=sig1+sig2+sig3;
 SIG=fft(sig);
 SIG=SIG(1:95);
 
+%Se : signal en sortie de ligne
 Se=ligne(SIG,h,6);
 figure(1)
 plot(abs(SIG))

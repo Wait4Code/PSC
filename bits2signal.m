7b5aa9f9510e4382debff0b9d77c8cfadb3c93fb
Anthony Roussel <anthony.roussel@insa-lyon.fr>
2016-10-04 14:45:45 +0200

    try to fix a bug in bits2signal

--- bits2signal.m
+++ bits2signal.m
@@ -1,22 +1,27 @@
-function x=bits2signal(sous_trame,tab,prefixe_cyclique)
+function x = bits2signal( sous_trame, tab, prefixe_cyclique )
+% Transforme la sous-trame numérique en les signaux analogiques modulé QAM
+%
+% Cette fonction prend un sous-trame en bits comme entrée et renvoie la
+% sous_trame en temps à transmettre dans les canaux
+%
+% arguments :
+% - sous_trame est un supertrame sans la partie synchronisation
+% - tab est le vecteur d'allocation de bits
+% - préfixe cyclique correspond le nombre de préfixe qu'on va ajouter au
+%   début du signal à transmettre
+%
 
-%cette fonction prend un sous-trame en bits comme entrée et renvoie la sous_trame en
-%temps à transmettre dans les canal.
+N = length( tab ); % nb de canaux
+v = prefixe_cyclique; % taille de prefixe
 
-% sous_trame est un supertrame sans la parie synchronisaton.
-% tab est le vecteur d'allocation de bits
-% préfixe cyclique correspond le nombre de préfixe qu'on va ajouter au
-% début du signal à transmettre.
+suite_symb = zeros( 1, N );
 
-N=length(tab); % nb de canaux
-v=prefixe_cyclique; %taille de prefixe
+k = 1;
 
-suite_symb=[];
-k=1;
-for i=1:N
-    suite_symb(i) = codage_symb(sous_trame(k:k+tab(i)-1),2^(tab(i)));
-    suite_symb(i) = modulationQAM(suite_symb(i),2^(tab(i)));
-    k=k+tab(i);
+for i = 1:N
+  suite_symb(i) = codage_symb( sous_trame( k:k+tab(i)-1 ), 2^( tab(i) ) );
+  suite_symb(i) = modulationQAM( suite_symb(i), 2^( tab(i) ) );
+  k = k + tab(i);
 end
 
-x=modulationDMT(suite_symb,N,v);
+x = modulationDMT( suite_symb, N, v );

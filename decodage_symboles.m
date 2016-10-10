88bfb4396c6ad80a525c8c475e92337183ec1524
Anthony Roussel <anthony.roussel@insa-lyon.fr>
2016-10-10 14:46:31 +0200

    encoding of decodage_symboles file were not correct

--- decodage_symboles.m
+++ decodage_symboles.m
@@ -1,18 +1,17 @@
-function suite_bits=decodage_symboles(suite_symboles,M)
+function suite_bits = decodage_symboles( suite_symboles, M )
 
-% La fonction decodage_symboles permet de décoder une suite de symboles pour
-% obtenir la suite binaire associée.
+% La fonction decodage_symboles permet de dÃ©coder une suite de symboles pour
+% obtenir la suite binaire associÃ©e.
 
-% suite_symboles est la suite des symboles sous forme décimale.
-% M est l'indice de modulation utilisé pour coder cette suite.
-
+% suite_symboles est la suite des symboles sous forme dÃ©cimale.
+% M est l'indice de modulation utilisÃ© pour coder cette suite.
 
 %%%%%%%%%%%%%%%%%%
 % Initialisation %
 %%%%%%%%%%%%%%%%%%
 
 k=log2(M); % k est le nombre de bits par symbole (M=2^k)
-longs=length(suite_symboles); % Nombre de symboles en entrée
+longs=length(suite_symboles); % Nombre de symboles en entrÃ©e
 longb=longs*k; % Nombre de bits en sortie 
 string_bits=[]; % Initialisation de la 'string' qui contiendra la suite de bits 
 
@@ -27,7 +26,7 @@ for i=1:longs % On parcourt la suite de symboles
 end
 
 % Comme la fonction 'dec2bin' renvoie une structure de type 'string' on va
-% transformer le résultat en vecteur numérique.
+% transformer le rÃ©sultat en vecteur numÃ©rique.
 suite_bits=[]; % Initialisation du vecteur qui contiendra la suite de bits 
 for j=1:length(string_bits)
     suite_bits(j)=str2num(string_bits(j)); % La fonction dec2bin renvoie une 'string' qu'on transforme en vecteur

d116a5b6a21c4ad20a496d99e655f04fe9293091
Anthony Roussel <anthony.roussel@insa-lyon.fr>
2016-10-10 14:43:45 +0200

    change allocation bits function to hava number of sub-channels from snr array

--- allocation_bits.m
+++ allocation_bits.m
@@ -1,28 +1,31 @@
-function b= allocation_bits(SNR)
+function b = allocation_bits( snr )
+% fonction d'allocation de bits en fonction du SNR
+%
+% inputs :
+% - snr est un tableau ligne de snr pour les n canaux
+%
+% output is a table of number of bits allocated for each sub-channel
+%
+% example :
+%
+%     b = allocation_bits( [ 5 10 12 ] )
 
-    % config
-    snr_gap = 10^( 9.5 / 10 ); % autour de 9.5 db pour Pe=10e-7 sur une QAM
-    gamma = 10^( 6 / 10 ); % autour de 6 db environ : La marge du rapport signal sur bruit
-    nb_canaux = 4;
+% configuration
+snr_gap = 10^( 9.5 / 10 ); % autour de 9.5 db pour Pe=10e-7 sur une QAM
+gamma = 10^( 6 / 10 ); % autour de 6 db environ : La marge du rapport signal sur bruit
+nb_canaux = length( snr );
 
-    % number of bits per channel
-    b = [];
+% number of bits per channel
+b = zeros( 1, nb_canaux );
 
-    for i = 1:nb_canaux
-      b( i ) = round( log2( 1 + SNR( i ) / ( snr_gap * gamma ) ) );
+for i = 1:nb_canaux
+  b( i ) = round( log2( 1 + snr( i ) / ( snr_gap * gamma ) ) );
 
-      if b( i ) < 0
-        b( i ) = 0;
-        
-      elseif b( i ) > 15
-        b( i ) = 15;
-        
-      end
-    end
-    
-    
-    disp( b );
-    
-    %plot( 1:nb_canaux, b );
-    
+  if b( i ) < 0
+    b( i ) = 0;
+  elseif b( i ) > 15
+    b( i ) = 15;
+  end
 end
+
+disp( b );

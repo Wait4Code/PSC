7f0f752550173bdc1ad6ad3a289d0e089fdd3513
Baudringhien Guillaume <gbaudringh@TC405-009-01.insa-lyon.fr>
2016-10-10 14:29:44 +0200

    Codage / Decodage RS pour symbole

---
+++ codage_RS_test.m
@@ -0,0 +1,12 @@
+enc = comm.RSEncoder(5,3); %RSEncoder( 5, sqrt( length( msg ) ) )
+enc.BitInput=true;
+msg = [0; 0; 1; 1; 0; 0; 1; 1; 0; 0; 1; 1; 0; 0; 1; 0; 0; 1; 1; 0; 0; 1; 1; 0; 0; 1; 1; 0; 0; 1; 0; 0; 1; 1; 0; 0; 1; 1; 0; 0; 1; 1; 0; 0; 1];
+cod = step( enc, msg );
+%disp(cod);
+disp(length(cod));
+%disp(enc);
+enco = comm.RSDecoder(5,3);
+enco.BitInput=true;
+deco = step( enco, cod );
+disp(deco);
+disp (deco == msg);

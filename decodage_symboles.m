bd75cee2f65d15d4d881a971e9cdeeb2bd8ee88a
Anthony Roussel <anthony.roussel@insa-lyon.fr>
2016-10-11 22:08:58 +0200

    add comments for decodage symboles
    - and fix a performance issue for decodage symbole ( see matlab documentation )

--- decodage_symboles.m
+++ decodage_symboles.m
@@ -1,5 +1,15 @@
 function suite_bits = decodage_symboles( symb_deci, M )
-
+% Décodage d'un symbole
+%
+% inputs :
+% - symb_deci est un symbole au format décimal
+% - M est le nombre de symboles possibles sachant que M=2^n
+%
+% output correspond à la suite de bits décodées à partir du symbole
+%
+% example :
+% decodage_symboles( 5, 4 )
+%
 % Dans la version de prof, on travaille sur un tableau de symbole décimale,
 % vue que pour chaque canal on transmettre qu'un seul symbole, du coup on
 % peut simplifier la fonction en prenant un symbole décimal comme la
@@ -21,7 +31,7 @@ string_bits = [ string_bits b ];
 
 % Comme la fonction 'dec2bin' renvoie une structure de type 'string' on va
 % transformer le résultat en vecteur numérique.
-suite_bits = []; % Initialisation du vecteur qui contiendra la suite de bits
+suite_bits = zeros( 1, length( string_bits ) ); % Initialisation du vecteur qui contiendra la suite de bits
 for j = 1:length( string_bits )
-    suite_bits(j) = str2num( string_bits(j) ); % La fonction dec2bin renvoie une 'string' qu'on transforme en vecteur
+  suite_bits(j) = str2num( string_bits(j) ); % La fonction dec2bin renvoie une 'string' qu'on transforme en vecteur
 end

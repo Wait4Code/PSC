10e8eecf9e2e1cb36b40dd127dce2d93abd91336
Anthony Roussel <anthony.roussel@insa-lyon.fr>
2016-10-11 21:49:23 +0200

    add simple example and header for codage canal
    /!\ this script is NOT working
    
    see this bug when example is played :
    
    ```
    Error using horzcat
    Dimensions of matrices being concatenated are not consistent.
    
    Error in interleaver (line 20)
    msg = [ msg zeros( 1, delay ) ];
    
    Error in codage_canal (line 34)
      interleaved = interleaver( trame_rs, 3, 2 );
    ```

--- codage_canal.m
+++ codage_canal.m
@@ -1,6 +1,21 @@
 function suite_bits_codee = codage_canal( suite_bits, generateur_crc, type_buffer )
+% Réalise le codage de canal pour une suite de bits donné
+%
+% Cette suite de bits sera codé CRC, RS puis entrelacé
+% selon les paramètres spécifiés en entrée
+%
+% inputs :
+% - suite_bits correspond à une suite de bits
+% - generateur_crc correspond à la matrice génératrice du codeur CRC
+% - type_buffer doit valoir 1 si on veut réaliser l'entralacement
+%
+% output : suite de bits codée de taille ??
+%
+% example :
+% codage_canal( [ 0 1 0 1 1 1 0 0 0 ], [ 1 0 1 1 1 0 0 0 1 ], 1 )
+%
 
-% suite_bits_codee=codage_canal( [ 0 1 0 1 1 0 1 1 ], [ 1 ], 1)
+% suite_bits_codee = codage_canal( [ 0 1 0 1 1 0 1 1 ], [ 1 ], 1)
 
 trame = codage_crc( suite_bits, generateur_crc );
 taille_trame = length( trame );

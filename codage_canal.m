733f4aededb7120947fd9dd1fa003f6c4308733c
Anthony Roussel <anthony.roussel@insa-lyon.fr>
2016-10-11 20:29:51 +0200

    fix typo in channel coding

--- codage_canal.m
+++ codage_canal.m
@@ -1,28 +1,29 @@
-function suite_bits_codee=codage_canal(suite_bits,generateur_crc,type_buffer)
+function suite_bits_codee = codage_canal( suite_bits, generateur_crc, type_buffer )
 
-trame= codage_crc(suite_bits,generateur_crc);
-taille_trame=length(trame);
+% suite_bits_codee=codage_canal( [ 0 1 0 1 1 0 1 1 ], [ 1 ], 1)
+
+trame = codage_crc( suite_bits, generateur_crc );
+taille_trame = length( trame );
 
 % reed-solomon
-N_rs=floor(taille_trame/(8*224);
+N_rs = floor( taille_trame / ( 8 * 224 ) );
 
-trame_rs=[];
+trame_rs = [];
 
-if(N_rs>0)
-    for i=1:N_rs
-        encoded = rs_encoding( trame_init', 240, 224 );
-        trame_rs=[trame_rs encoded'];
-    end
+if N_rs > 0
+  for i = 1:N_rs
+    encoded = rs_encoding( trame_init', 240, 224 );
+    trame_rs = [ trame_rs encoded' ];
+  end
 else
-    trame_rs=trame;
+    trame_rs = trame;
 end
-trame_final=trame_rs;
+trame_final = trame_rs;
 
-if(type_buffer=1)
-% interleaver
-interleaved = interleaver(trame_rs, 3, 2 );
+if type_buffer == 1
+  interleaved = interleaver( trame_rs, 3, 2 );
 end
 
-trame_final=interleaved
+trame_final = interleaved;
 
 suite_bits_codee = trame_final;

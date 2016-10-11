daaa077f2f92b088b2e33078f2a1ff29c111444f
Anthony Roussel <anthony.roussel@insa-lyon.fr>
2016-10-11 19:01:12 +0200

    no end keyword at end of matlab function

--- desassemblage_supertrame.m
+++ desassemblage_supertrame.m
@@ -1,7 +1,5 @@
-function suite_bits_final= desassemblage_supertrame(suite_bits_supertrame, generateurCRC)
+function suite_bits_final = desassemblage_supertrame( suite_bits_supertrame, generateurCRC)
 
-    trame_deinterleaved = deinterleaver(suite_bits_supertrame, 1 , 4);
-    trame = rs_decoding( trame_deinterleaved', 5, 3 );
-    suite_bits_final= decodage_crc(trame, generateurCRC);
-
-end
+trame_deinterleaved = deinterleaver( suite_bits_supertrame, 1, 4 );
+trame = rs_decoding( trame_deinterleaved', 5, 3 );
+suite_bits_final = decodage_crc( trame, generateurCRC );

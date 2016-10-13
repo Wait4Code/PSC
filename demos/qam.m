23d07b05ea90dc8e737b1676ad14405205139b17
Anthony Roussel <anthony.roussel@insa-lyon.fr>
2016-10-13 18:06:18 +0200

    add demo for qam

---
+++ demos/qam.m
@@ -0,0 +1,13 @@
+addpath( '..' );
+
+M = 16;
+
+y = zeros( 1, M );
+for i = 0:( M-1 )
+  y( i+1 ) = modulationQAM( i, M );
+end
+
+figure;
+plot( y, 'x' );
+axis( [ -200 200 -200 200 ] );
+title( 'Constellation de la 16-QAM' );

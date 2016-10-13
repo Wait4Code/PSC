4caed8e7a618e33c5d31f6d33eb4e6bbe0bdfabd
Anthony Roussel <anthony.roussel@insa-lyon.fr>
2016-10-13 15:29:48 +0200

    add demo from source generator

---
+++ demos/generateur_source.m
@@ -0,0 +1,13 @@
+% Demo du générateur de source
+addpath( '..' );
+
+x = gene_bits( 250, 0.5 );
+
+figure;
+subplot( 2, 1, 1 );
+plot( x );
+title( 'Signal aléatoire binaire' );
+
+subplot( 2, 1, 2 );
+histogram( x ); 
+title( 'Histogramme' );
